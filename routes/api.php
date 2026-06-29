aba35a64337844f6a9a13d107f95ce79929b7e0e
pheaktra1402 <pheaktrathorn474@email.com>
2026-06-29 10:25:30 +0700

    updated

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
-
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::get('/users', function () {
     return ['name' => 'Pheaktra', 'email' => 'pheaktra@gmail.com'];
-});
+    });
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
