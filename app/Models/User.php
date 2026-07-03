e6fd50767e8ebccb74a05d69cc236e6bb541c128
pheaktra1402 <pheaktrathorn474@email.com>
2026-07-03 08:57:32 +0700

    api updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -16,6 +17,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+     use HasApiTokens, HasFactory, Notifiable; 
 
     /**
      * Get the attributes that should be cast.
@@ -30,3 +32,4 @@ class User extends Authenticatable
         ];
     }
 }
+
